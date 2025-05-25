80e4240a1621aea7d63a6657e124aa43a44c3883
Zabir Arkam <zabirarkam27@gmail.com>
2025-05-25 14:37:11 +0600

    added delete functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cuvfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // MongoDB Client Config
 const client = new MongoClient(uri, {
@@ -36,45 +35,60 @@ async function run() {
       res.send(result);
     });
 
+    // User-specific ManageService
+    app.get("/my-services", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        return res.status(400).send({ error: "Email query is required" });
+      }
 
+      const result = await servicesCollection
+        .find({ providerEmail: email })
+        .toArray();
+      res.send(result);
+    });
 
+    // Single service by ID
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const service = await servicesCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(service);
+    });
 
+    // Add new service
+    app.post("/services", async (req, res) => {
+      const newService = req.body;
+      const result = await servicesCollection.insertOne(newService);
+      res.send(result);
+    });
 
+    //Delete service
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const result = await servicesCollection.deleteOne({ _id: new ObjectId(id) });
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    })
 
+    app.get("/", (req, res) => {
+      res.send("Gadget aid server is running");
+    });
 
+    app.listen(port, () => {
+      console.log(`Gadget Aid server is running on port:  ${5000}`);
+    });
 
-
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Gadget aid server is running')
-})
-
-app.listen(port, () => {
-    console.log(`Gadget Aid server is running on port:  ${5000}`)
-})
